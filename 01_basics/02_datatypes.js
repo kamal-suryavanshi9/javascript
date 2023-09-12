c00e691341fcae6dad75a5f6460695c30e03a624
kamal-suryavanshi9 <126935430+kamal-suryavanshi9@users.noreply.github.com>
2023-09-12 17:47:07 +0000

    data types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+// alert (3+3) // we are using nodejs, not browser
+
+let name = "Kamal"
+let age = 33
+let isLoggedIn = false
+
+/*
+number => 2**53
+bigint
+string => ""
+boolean ==> true/false
+null => standalone value
+undefined => value not assigned to variable
+symbol => unique
+
+object => 
+*/
+
+console.log(typeof(age));
+console.log(typeof(null));
+console.log(typeof(undefined));
