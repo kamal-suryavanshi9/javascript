df83e4c9bd13f0f22bd4cadf51e922e3f8ddd796
kamal-suryavanshi9 <126935430+kamal-suryavanshi9@users.noreply.github.com>
2023-09-12 18:22:35 +0000

    operations and conversions

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,59 @@
+/*
+let score = "33abc"
+
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// '33' => 33
+// '33abc'=> NaN
+// true => 1; false => 0
+
+
+let isLoggedIn
+console.log(isLoggedIn);
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log("booleanIsLoggedIn");
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
+
+
+// 1 => true; 0 => false
+// "" => false
+// "Kamal" => true
+
+let stringNumber = String('34');
+
+console.log(stringNumber)
+console.log(typeof stringNumber)
+*/
+
+// ************************************ Operations *****************************************************
+
+let value =3
+let negValue = -value
+
+console.log(negValue);
+
+// + - * ** / %
+
+let str1 = "Hello"
+let str2 = " Kamal"
+let str3 = str1 + str2
+
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + '2');
+let summ = 1 + 2 + '2';
+
+console.log(typeof summ);
