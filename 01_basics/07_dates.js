6c75eab19fb6207a6047771432370f647a57ec0a
kamal-suryavanshi9 <126935430+kamal-suryavanshi9@users.noreply.github.com>
2023-09-23 16:18:32 +0000

    dates

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,29 @@
+let myDate = new Date()
+
+// console.log(myDate);
+// console.log(myDate.toDateString());
+// console.log(typeof(myDate));
+
+let newDate = new Date(2023, 0, 1, 13, 34)
+
+console.log(newDate.toDateString());
+console.log(newDate.toLocaleString());
+
+
+let myBday = new Date("1988-01-04")
+
+// console.log(myBday.toLocaleString());
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myBday.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+console.log(newDate.getDate());
+console.log(newDate.getDay());
+console.log(newDate.getMonth());
+
+console.log(ß
+    newDate.toLocaleString('default', {
+        weekday: "long",
+    }))
