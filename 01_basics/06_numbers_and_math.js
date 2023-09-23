62ece5576b643da7012415a61bdba8ddc0f1c6f8
kamal-suryavanshi9 <126935430+kamal-suryavanshi9@users.noreply.github.com>
2023-09-23 15:55:23 +0000

    numbers and math

---
+++ 01_basics/06_numbers_and_math.js
@@ -0,0 +1,35 @@
+const score = 4000
+
+const balance = new Number(1000000000)
+
+// console.log(score);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(score.toFixed(3));
+
+
+const otherNumber = 213.433535
+// console.log(otherNumber.toPrecision(2));
+
+// console.log(balance.toLocaleString('en-IN'));
+
+// +++++++++++++++++++++++++++++ Maths +++++++++++++++++++++++++++
+
+console.log(Math);
+
+// console.log(Math.abs(-3));
+// console.log(Math.round(3.356));
+// console.log(Math.ceil(3.356));
+// console.log(Math.floor(3.356));
+// console.log(Math.random());
+// console.log(Math.random());
+// console.log(Math.random());
+// console.log(Math.random());
+
+const min = 12
+const max = 14
+console.log(Math.random()*6 + 1 );
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
